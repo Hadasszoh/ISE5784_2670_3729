69d8acbd4695897346ad1c1bbc5e322d90730531
Hadasszoh <hzohar@g.jct.ac.il>
2024-05-22 10:07:02 +0300

    stage 3

---
+++ src/unittests/primitives/RayTest.java
@@ -0,0 +1,11 @@
+package unittests.primitives;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import primitives.Ray;
+import primitives.Point;
+import primitives.Vector;
+
+class RayTest {
+}
